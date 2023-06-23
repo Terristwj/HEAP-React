98a54aeb6c6872c19662846d3d569b178d8413d7
Terris <27739101+Terristwj@users.noreply.github.com>
2023-06-23 19:32:35 +0800

    Ex1 Template

---
+++ README.md
@@ -0,0 +1,13 @@
+## HEAP-React
+
+React Workshop by great SMU seniors
+
+---
+
+## Reference
+
+1. https://gaurav-me.github.io/basics-react
+
+---
+
+## Setup
---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "https://"
+
+foo = 5 + 2;
+console.log("This is foo:" + foo);
+
+//  ---
+
+const App = () => {
+  return "Something";
+};
+
+// App vs App()
+// Pointing to a pointer VS pointing to a function
+console.log(App);
+console.log(App());
